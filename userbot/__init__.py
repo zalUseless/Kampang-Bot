01b863c84d85f72bd88d90445da50a153596bf75
Koala <72527434+ManusiaRakitan@users.noreply.github.com>
2021-02-19 22:51:11 +0800

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -345,7 +345,7 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline("{} {}".format("🔹", x), data="ub_modul_{}".format(x))
+        custom.Button.inline("{} {}".format("✲", x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols],
@@ -420,7 +420,7 @@ with bot:
                         [
                             custom.Button.url(
                                 "GitHub Repo",
-                                "https://github.com/Camel07/XBot-Remix"),
+                                "https://github.com/ManusiaRakitan/Kampang-Bot"),
                             custom.Button.url(
                                 "Support",
                                 "https://t.me/mixiologist")],
