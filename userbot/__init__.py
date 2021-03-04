7a5a4256fe980a58b2e4f617cc28b84cf910f325
Koala <72527434+ManusiaRakitan@users.noreply.github.com>
2021-03-05 02:19:50 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -146,6 +146,9 @@ ANTI_SPAMBOT_SHOUT = sb(os.environ.get("ANTI_SPAMBOT_SHOUT", "False"))
 # Youtube API key
 YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
 
+# Custom Handler
+CMD_HANDLER = os.environ.get("CMD_HANDLER", None)
+
 # Default .alive name
 ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
 
