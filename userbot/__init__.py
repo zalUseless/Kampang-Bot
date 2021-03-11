1f4d88f0b72eac0497295e56673a838ec2db285c
tungauicipiyey <80462153+tungauicipiyey@users.noreply.github.com>
2021-03-11 18:45:35 +0000

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -100,7 +100,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/ManusiaRakitan/Kampang-Bot.git")
+    "https://github.com/tungauicipiyey/kampang -Bot.git")
 UPSTREAM_REPO_BRANCH = os.environ.get(
     "UPSTREAM_REPO_BRANCH", "Kampang")
 
