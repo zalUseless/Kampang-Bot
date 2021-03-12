493a89cc34b545ae7c1c0a79666696a9126830ee
tungauicipiyey <80462153+tungauicipiyey@users.noreply.github.com>
2021-03-12 18:23:19 +0000

    Create system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -233,13 +233,14 @@ async def amireallyalive(alive):
     await get_readable_time((time.time() - StartTime))
     output = (
         f"**╭▻►▻►▻►▻►▻►◄◅◄◅◄◅◄◅╮**\n"       
-        f"     ...**╰▻ 𝐁𝐨𝐭 𝐕𝐢𝐫𝐮𝐬 ◅╯**...\n\n"               
+        f"     .....**╰▻ 𝐁𝐨𝐭 𝐕𝐢𝐫𝐮𝐬 ◅╯**.....\n\n"               
         f"➨⚔ **𝚃𝚑𝚎 𝙺𝚒𝚗𝚐**  :  ➬ `{DEFAULTUSER}` \n"
         f"➨⛓ **𝚄𝚜𝚎𝚛𝚗𝚊𝚖𝚎** :  ➬ `@{user.username}` \n"
         f"➨🐍 **𝙿𝚢𝚝𝚑𝚘𝚗**     :  ➬ `Versi {python_version()}` \n"
         f"➨✔ **𝚅𝚎𝚛𝚜𝚒𝚘𝚗𝚗**  :  ➬ `{BOT_VER}` \n"     
+        f"➨🔱 **Pemilik**   :   ➬ **[Pemilik](t.me/DamnBitchh)** \n"
         f"**╰▻►▻►▻►▻►▻►◄◅◄◅◄◅◄◅╯** \n"
-       f" **[Grup Oficial](t.me/CariTemen_Random)**   |   **[Pemilik](t.me/DamnBitchh)**   |   **[Instagram](https://www.instagram.com/arimulyaa_/)** ")     
+        f" **[Grup Oficial](t.me/CariTemen_Random)**     |     |    **[Instagram](https://www.instagram.com/arimulyaa_/)** ")     
       
 
         
