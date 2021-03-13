0b5a46c8b7f9201a16da47af9efdcfac70a3fe43
tungauicipiyey <80462153+tungauicipiyey@users.noreply.github.com>
2021-03-13 06:01:01 +0000

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -234,13 +234,14 @@ async def amireallyalive(alive):
     output = (
         f"**╭▻►▻►▻►▻►▻►◄◅◄◅◄◅◄◅╮**\n"       
         f"     ......**╰▻ 𝐁𝐨𝐭 𝐕𝐢𝐫𝐮𝐬 ◅╯**......\n\n"               
-        f"➨⚔ **𝚃𝚑𝚎 𝙺𝚒𝚗𝚐**  :  ➟ `{DEFAULTUSER}` \n"
-        f"➨⛓ **𝚄𝚜𝚎𝚛𝚗𝚊𝚖𝚎** :  ➟ `@{user.username}` \n"
-        f"➨🐍 **𝙿𝚢𝚝𝚑𝚘𝚗**     :  ➟ `Versi {python_version()}` \n"
+        f"➨⚔ **𝚃𝚑𝚎 𝙺𝚒𝚗𝚐**  : ➟ `{DEFAULTUSER}` \n"
+        f"➨⛓ **𝚄𝚜𝚎𝚛𝚗𝚊𝚖𝚎**: ➟ `@{user.username}` \n"
+        f"➨🐍 **𝙿𝚢𝚝𝚑𝚘𝚗**    :   ➟ `Versi {python_version()}` \n"
+        f"➨🖥 **𝚃𝚎𝚕𝚎𝚝𝚑𝚘𝚗** :  ➟ `Versi {version.__version__}` \n"
         f"➨✔ **𝚅𝚎𝚛𝚜𝚒𝚘𝚗**  :  ➟ `{BOT_VER}` \n"     
-        f"➨🔱 **Pemilik**      :   ➟ **[TUNGAU](t.me/DamnBitchh)** \n"
+        f"➨🔱 **Pemilik**      :  ➟ **[TUNGAU](t.me/DamnBitchh)** \n"
         f"**╰▻►▻►▻►▻►▻►◄◅◄◅◄◅◄◅╯** \n"
-        f" **[Grup Oficial](t.me/CariTemen_Random)**      |-----|     **[Instagram](https://www.instagram.com/arimulyaa_/)** ")     
+        f" **| [Grup Oficial](t.me/CariTemen_Random)**      |-----|     **[Instagram](https://www.instagram.com/arimulyaa_/) |** ")     
       
 
         
