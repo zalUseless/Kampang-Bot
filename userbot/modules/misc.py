db404a7247ef571c7cf5f5846ecc009f68f28ab3
tungauicipiyey <80462153+tungauicipiyey@users.noreply.github.com>
2021-03-15 14:53:47 +0000

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2019 The Raphielscape Company LLC.
-#
+# Ported For Lord-Userbot By liualvinas/Alvin
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
@@ -141,6 +141,19 @@ async def _(event):
     if event.reply_to_msg_id:
         await event.get_reply_message()
     tap = await bot.inline_query(botusername, wwwspr)
+    await tap[0].click(event.chat_id)
+    await event.delete()
+
+
+@register(outgoing=True, pattern=r"^\.xogame(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    botusername = "@xobot"
+    noob = "play"
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    tap = await bot.inline_query(botusername, noob)
     await tap[0].click(event.chat_id)
     await event.delete()
 
