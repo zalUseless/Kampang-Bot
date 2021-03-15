a14e9e931b2f0900194237cb44949d2cf9917f50
tungauicipiyey <80462153+tungauicipiyey@users.noreply.github.com>
2021-03-15 17:30:47 +0000

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -93,11 +93,11 @@ async def pingme(pong):
     end = datetime.now()
     duration = (end - start).microseconds / 1000
     await pong.edit(f"**╭▻•╰▻𝙲𝙾𝚁𝙾𝙽𝙰 𝚅𝙸𝚁𝚄𝚂◅╯•◅╮**\n"
-                    f"**⌲Ping Virus :** "
-                    f"➥ `%s𝖒𝖘` \n"
-                    f"**⌲Virus Aktif :** "
-                    f"➥ `{uptime}` Dangerr\n"
-                    f"**⌲𝐌𝐚𝐣𝐢𝐤𝐚𝐧 :** `{ALIVE_NAME}`" % (duration))
+                    f"⌲ **Ping Virus:** "
+                    f"`%s𝖒𝖘` \n"
+                    f"⌲ **Virus Aktif:** "
+                    f"`{uptime}` Dangerr \n"
+                    f"⌲**𝐌𝐚𝐣𝐢𝐤𝐚𝐧 :** `{ALIVE_NAME}`" % (duration))
                     
 
 
