b0557a439b7e1d0f85fe4bf6a510a29e8592c1d1
tungauicipiyey <80462153+tungauicipiyey@users.noreply.github.com>
2021-03-16 04:10:33 +0000

    Update gabut.py

--- userbot/modules/gabut.py
+++ userbot/modules/gabut.py
@@ -163,14 +163,14 @@ CMD_HELP.update({
 \nUsage: tipu tipu anjeeeng.\
 \n\n`L`\
 \nUsage: Untuk Menjawab Salam.\
-"\n\n>`.sagapung`\
+\n\n>`.sagapung`\
     \nUsage: liat aja anak anak tolol.\
-"\n\n>`.cabean`\
+\n\n>`.cabean`\
     \nUsage: liat aja anak anak tolol.\
-"\n\n>`ass`"
+\n\n>`ass`"
     \nUsage: melakukan salam.\
-"\n\n>`keping`\
+\n\n>`keping`\
     \nUsage: kecepatan ping.\
-"\n\n>`G`\
+\n\n>`G`\
     \nUsage: pantun goblok haha."
 })
